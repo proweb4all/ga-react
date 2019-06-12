9e097961d33025e70d97ce6001e1564b2f16ca9c
ProWeb4All <proweb4all@narod.ru>
2019-06-12 06:23:45 +0300

    lesson 3 / hard

--- my-app/src/components/post-list-item/post-list-item.js
+++ my-app/src/components/post-list-item/post-list-item.js
@@ -4,9 +4,10 @@ import './post-list-item.css';
 const PostListItem = () => {
     return (
         <li className='app-list-item d-flex justify-content-between'>
-            <span className='app-list-item-label'>
-                Какой-то пост...
-            </span>
+            <div className='app-list-item-label'>
+                <p className='item-date'>12.06.2019</p>
+                <p>Какой-то пост...</p>
+            </div>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type='button' className='btn-star btn-sm'>
                     <i className='fa fa-star'></i>
